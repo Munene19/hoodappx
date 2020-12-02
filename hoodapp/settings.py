32451d792059e1957e83c6ea960830fba89fa740
Mike Munene <you@example.com>
2020-12-02 09:19:21 +0300

    settings updated

--- hoodapp/settings.py
+++ hoodapp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'hoodapp'
+    'hood'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
